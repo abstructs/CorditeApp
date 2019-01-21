c40ce49fc6af5e74bbb41b0bc5d8d090f6f6d928
abstructs <andrewclarke540@gmail.com>
2019-01-21 00:17:11 -0500

    implement logout

--- app/app/src/main/java/com/cordite/cordite/HomeActivity.java
+++ app/app/src/main/java/com/cordite/cordite/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.cordite.cordite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +14,7 @@ import android.view.View;
 
 import com.cordite.cordite.Map.MapsActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -25,18 +29,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-
-//        setupToolbar();
-
+        setupToolbar();
+        setupButtons();
     }
 
     private void setupButtons() {
@@ -52,6 +46,19 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    private void logout() {
+        SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.shared_preferences_key),
+                Context.MODE_PRIVATE).edit();
+
+        editor.remove("token");
+
+        editor.commit();
+
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
@@ -60,16 +67,34 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void setupToolbar() {
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//
-//        setSupportActionBar(toolbar);
-//
-//        if(getSupportActionBar() != null) {
-//            getSupportActionBar().setTitle("Home");
-//        }
-//
-//        toolbar.setSubtitle("Subtitle");
-//        toolbar.inflateMenu(R.menu.main_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+
+                mDrawerLayout.closeDrawers();
+
+                switch(menuItem.getItemId()) {
+                    case R.id.logout:
+                        logout();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
--- app/app/src/main/java/com/cordite/cordite/MainActivity.java
+++ app/app/src/main/java/com/cordite/cordite/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
 
             startActivity(intent);
+
+            finish();
             }
         });
 

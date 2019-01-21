53e435353e544bc988709718b4a96ccd00a247f1
abstructs <andrewclarke540@gmail.com>
2019-01-21 00:05:34 -0500

    add toolbar with drawer

--- app/app/src/main/java/com/cordite/cordite/HomeActivity.java
+++ app/app/src/main/java/com/cordite/cordite/HomeActivity.java
@@ -5,22 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.cordite.cordite.Map.MapsActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+//        setupToolbar();
+
+    }
+
+    private void setupButtons() {
         FloatingActionButton trackFab = findViewById(R.id.trackFab);
 
         trackFab.setOnClickListener(new View.OnClickListener() {
@@ -31,9 +50,6 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        setupToolbar();
-
     }
 
     @Override
@@ -56,4 +72,13 @@ public class HomeActivity extends AppCompatActivity {
 //        toolbar.inflateMenu(R.menu.main_menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
